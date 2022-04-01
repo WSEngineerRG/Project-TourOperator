165708a55479ebc050e4019dfe189854a9bb9713
RGDev <95212617+WSEngineerRG@users.noreply.github.com>
2022-04-01 12:24:18 +0200

    update then !

--- Class/manager.php
+++ Class/manager.php
@@ -2,5 +2,24 @@
 
 class manager
 {
+    public string $classname = 'manager';
+    public mixed $db;
+    const DB_HOSTNAME = 'localhost';
+    const DB_USERNAME = 'root';
+    const DB_PASSWORD = 'root';
+    const DB_NAME = 'comparo_full';
 
+    public function __construct()    // Constructor
+    {
+        $this->db = new mysqli(self::DB_HOSTNAME, self::DB_USERNAME, self::DB_PASSWORD, self::DB_NAME);
+        if ($this->db->connect_errno) {
+            echo "Failed to connect to MySQL: (" . $this->db->connect_errno . ") " . $this->db->connect_error;
+        }
+    }
+    public function db_connect(){
+       return $this->db;
+    }
 }
+
+$manager = new manager();
+
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: 'class',
+  // ...
+}
+const carousel = new Carousel(items, options);
+
+carousel.options = {
+  activeItemPosition: 1,
+  interval: 5000,
+}
