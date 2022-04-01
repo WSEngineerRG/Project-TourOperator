333ea8b7b4eed9c8371f7e9e9883b0b817c8eb58
prince <prince-benjamin@hotmail.com>
2022-04-01 12:17:12 +0200

    class

--- Class/destination.php
+++ Class/destination.php
@@ -2,5 +2,24 @@
 
 class destination
 {
+    private $id;
+    private $location;
+    private $price;
+
+    function __construct($data)
+    {
+        
+    }
+    function getId(){
+
+    }
+
+    function getLocation(){
+
+    }
+
+    function getPrice(){
+        
+    } 
 
 }
--- Class/review.php
+++ Class/review.php
@@ -3,4 +3,26 @@
 class review
 {
 
+    private $id;
+    private $message;
+    private $author;
+
+    function __construct($data)
+    {
+        
+    }
+
+    function getId(){
+
+    }
+
+    function getVAlue(){
+
+    }
+
+    function getAuthor(){
+
+    }
+
+    
 }
