c4450f0164ea06b8353953da61d912b12e3fdd48
prince <prince-benjamin@hotmail.com>
2022-04-01 12:17:12 +0200

    class

--- Class/tour_operator.php
+++ Class/tour_operator.php
@@ -1,6 +1,37 @@
 <?php
 
+
+
+
 class tour_operator
 {
+private $id;
+private $name;
+private $link;
+private $certificate;
+private $destinations;
+private $reviews;
+private $scores;
+
+
+function __construct($data){
+    
+}
+
+function getId(){
 
 }
+
+function getName(){
+
+}
+
+function getGrade(){
+
+}
+
+function isPremium(){
+    
+}
+}
+
